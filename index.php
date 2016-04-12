4b56e04c8ca7b82b3bc0484d5e8f5edfd0fc6fc6
cosmosgc <cosmoskitsune@hotmail.com>
2016-04-12 10:18:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+                <style>
+			body{
+			background: #fd746c; /* fallback for old browsers */
+			background: -webkit-linear-gradient(to left, #fd746c , #ff9068); /* Chrome 10-25, Safari 5.1-6 */
+			background: linear-gradient(to left, #fd746c , #ff9068); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+			}    
+        	</style>
 	</head>
 	<body class="container">	
 		<form action="login.php" method="post" accept-charset="UTF-8" class="form-group">
