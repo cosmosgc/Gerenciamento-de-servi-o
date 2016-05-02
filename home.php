2108f0a76c76dbb3c0bca771e3cf2cdf0f57ec4d
cosmosgc <cosmoskitsune@hotmail.com>
2016-05-02 16:11:13 -0300

    Added files via upload

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+//include("conectar.php");
+//include("var.php");
+?>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+          <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+            <style>
+            </style>
+	</head>
+<body>
+<nav class="navbar">
+      <ul class="nav navbar-nav">
+        <li><a href="cadastrarApp.php">Informações dos funcionarios</a></li>
+        <li><a href="demo.html">Informações dos serviços</a></li>
+      </ul>
+</nav>
+
