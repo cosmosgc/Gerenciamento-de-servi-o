45e725cc864387f3a4256fc4de3c016cadde8e47
cosmosgc <cosmoskitsune@hotmail.com>
2016-05-02 15:42:23 -0300

    Added files via upload

---
+++ var.php
@@ -0,0 +1,13 @@
+<?php
+$data= date("d/m/y");
+//$ip= $_SERVER[SERVER_ADDR];
+//$timeFloat= $_SERVER[REQUEST_TIME_FLOAT];
+
+session_start();
+$username=$_SESSION['username'];
+$password=$_SESSION['password'];
+$_SESSION['username']=$username;
+$_SESSION['password']=$password;
+$existeLogin=isset($_SESSION['username']);
+if($existeLogin==false){header("location:index.php");}        
+?>
