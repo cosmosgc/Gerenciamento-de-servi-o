ee1fe77287ae9f7dc9b02aa042009c80ba7d7a43
cosmosgc <cosmoskitsune@hotmail.com>
2016-05-02 15:45:02 -0300

    Added files via upload

---
+++ erro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Erro!
+        <?php
+            $erro = $_GET["erro"];
+            echo($erro);
+        ?>
+    </body>
+</html>
