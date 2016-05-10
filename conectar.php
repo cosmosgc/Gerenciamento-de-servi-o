d3e6c8e4e5c978cae9291cb93615e420d453ce6c
cosmosgc <cosmoskitsune@hotmail.com>
2016-05-10 09:41:23 -0300

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -1,7 +1,8 @@
 <?php 
 
 //Abrir a conexão com o banco
-$conexao = mysqli_connect("servidor", "usuario", "senha", "database");
+//$conexao = mysqli_connect("servidor", "usuario", "senha", "database");
+$conexao = mysqli_connect("mysql.hostinger.com.br", "u474781536_admin", "34421282", "u474781536_royal");
 if ($conexao == false) {
     $erro = mysqli_connect_error();
     header("location:erro.php?erro=$erro");
