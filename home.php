a39f005c526c986bb71fdc784493027dcdc481ff
cosmosgc <cosmoskitsune@hotmail.com>
2016-08-02 16:12:53 -0300

    Update home.php

--- home.php
+++ home.php
@@ -31,7 +31,7 @@ include("var.php");
 <html lang="en">
 
 <head>
-    <title><? echo($row["nome"]) ?></title>
+    <title><? echo($username) ?></title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="description" content="">
