e5bc6f429ac5237d42dae1ebe7b11cd357a750e4
cosmosgc <cosmoskitsune@hotmail.com>
2016-08-18 19:31:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 		var container = document.getElementById('container'),
 			renderer = new FSS.CanvasRenderer(),
 			scene = new FSS.Scene(),
-			light = new FSS.Light('#FFFFFF', '#FFAAAA'),
+			light = new FSS.Light('#FFFFFF', '#FF0022'),
 			geometry = new FSS.Plane(container.offsetWidth, container.offsetHeight, 6, 4),
 			material = new FSS.Material('#FFFFFF', '#FFFFFF'),
 			mesh = new FSS.Mesh(geometry, material),
