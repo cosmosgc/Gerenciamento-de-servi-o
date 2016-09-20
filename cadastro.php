4f5d1451bd267fc993afbb407cbed1cda26fc3a9
cosmosgc <cosmoskitsune@hotmail.com>
2016-09-20 09:24:39 -0300

    fix telefone

--- cadastro.php
+++ cadastro.php
@@ -249,9 +249,9 @@ if (isset($_GET["id_empresa"]))
 	</ul>
 	<!-- fieldsets -->
 	<fieldset>
-		<li>Empresa: <? echo($username)?></li>
-		<li>Telefone: <? echo($email); ?></li>
-		<li>Telefone: <? echo($tel); ?></li>
+		<h3>Empresa: <? echo($username)?></h3>
+		<h3>email: <? echo($email); ?></h3>
+		<h3>Telefone: <? echo($telefone); ?></h3>
 		<h2 class="fs-title">Crie a conta da empresa <? echo ($username);?></h2>
 		<h3 class="fs-subtitle">Essa é a etapa 1</h3>
 		<input type="text" name="email" placeholder="Email" required/>
