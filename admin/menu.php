097d930012963abe9aa373437f01bef7e46317b2
cosmosgc <cosmoskitsune@hotmail.com>
2016-10-11 16:42:08 -0300

    Update menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -17,7 +17,7 @@ debug_to_console($sql);
 $resultado = mysqli_query($conexao, 'SELECT * FROM empresa WHERE nome = "'.$username.'"' );
   iif (mysql_num_rows($resultado)==0) {
     $resultado = mysqli_query($conexao, 'SELECT empresa.nome id_empresa, cnpj, empresa.telefone, empresa.email, cidade, estado, desc_empresa FROM empresa, funcionario, setor WHERE funcionario.nome = $username AND fk_setor = id_setor AND funcionario.fk_empresa = id_empresa');
-      debug_to_console("logado como funcionario")
+      debug_to_console("logado como funcionario");
       if (!$resultado)
     {
         $erro = mysqli_error($conexao);
