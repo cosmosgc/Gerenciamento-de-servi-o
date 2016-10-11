24798e6c00315f22f0223c665c471ff3d12993f2
cosmosgc <cosmoskitsune@hotmail.com>
2016-10-10 22:11:23 -0300

    update admin
    php, design, funcionalidade

---
+++ admin/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+
+<?php include("menu.php");?>
+        <!-- page content -->
+		<div class="right_col" role="main">
+          <div class="">
+		<!-- widgets -->
+        
+            <div class="row top_tiles">
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-users"></i></div>
+                  <div class="count">179</div>
+                  <h3>membros</h3>
+                  <p>Empregados da empresa.</p>
+                </div>
+              </div>
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-comments-o"></i></div>
+                  <div class="count">179</div>
+                  <h3>Outputs</h3>
+                  <p>log em serviços.</p>
+                </div>
+              </div>
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-sort-amount-desc"></i></div>
+                  <div class="count">179</div>
+                  <h3>Serviços ativos</h3>
+                  <p>Quantidade de serviços faltando.</p>
+                </div>
+              </div>
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-check-square-o"></i></div>
+                  <div class="count">179</div>
+                  <h3>Serviços completos</h3>
+                  <p>Total de serviços completos.</p>
+                </div>
+              </div>
+            </div>
+		<!-- widgets -->
+
+		
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Site desenvolvido por Vin�cius Bretas Avezani de Mello Silva
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="scripts/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="scripts/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="scripts/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="scripts/nprogress/nprogress.js"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script src="scripts/custom.min.js"></script>
+  </body>
+</html>
