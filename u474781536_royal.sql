ea612bac7662925e52c1c2680a59901dcd9347ed
cosmosgc <cosmoskitsune@hotmail.com>
2016-10-18 09:13:27 -0200

    DB de 18 de outubro
    -estrutura mais concreta
    -alguns dados cadastrados
    -cadastros pelo hostinger

--- u474781536_royal.sql
+++ u474781536_royal.sql
@@ -1,28 +1,33 @@
--- MySQL dump 10.15  Distrib 10.0.20-MariaDB, for Linux (x86_64)
+
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: u474781536_royal
--- ------------------------------------------------------
--- Server version	10.0.20-MariaDB
+-- Servidor: localhost
+-- Tempo de Geração: 18/10/2016 às 11:11:18
+-- Versão do Servidor: 10.0.20-MariaDB
+-- Versão do PHP: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `empresa`
+-- Banco de Dados: `u474781536_royal`
 --
 
-DROP TABLE IF EXISTS `empresa`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `empresa` (
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `empresa`
+--
+
+CREATE TABLE IF NOT EXISTS `empresa` (
   `id_empresa` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `cnpj` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
@@ -33,77 +38,113 @@ CREATE TABLE `empresa` (
   `ceo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `cidade` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `estado` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_empresa`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`id_empresa`),
+  FULLTEXT KEY `desc_empresa` (`desc_empresa`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=22 ;
 
 --
--- Dumping data for table `empresa`
+-- Extraindo dados da tabela `empresa`
 --
 
-/*!40000 ALTER TABLE `empresa` DISABLE KEYS */;
-INSERT INTO `empresa` VALUES (1,'vinicius','cnpj','202cb962ac59075b964b07152d234b70','','cosmoskitsune@hotmail.com','','','',''),(2,'cosmosgc','061.480.139-75','864f1a5b1b39f5f071ca3123c4e806bb','4734421282','cosmoskitsune@hotmail.com','','','São Francisco do Sul','SC'),(3,'teste1','123456789','202cb962ac59075b964b07152d234b70','4734421282','cosmoskitsune@hotmail.com','empresa com coisas que começa com letra b','','São Francisco do Sul, SC',''),(4,'teste2','123456789','202cb962ac59075b964b07152d234b70','4734421282','cosmoskitsune@hotmail.com','Coisas','','SFS',''),(5,'teste3','123456789','202cb962ac59075b964b07152d234b70','4734421282','cosmoskitsune@hotmail.com','bush','','R. Francisco Mascarenhas\r\nReta',''),(6,'teste4','123456789','202cb962ac59075b964b07152d234b70','4734421282','cosmoskitsune@hotmail.com','bush','','R. Francisco Mascarenhas\r\nReta',''),(7,'testes','123456789','d41d8cd98f00b204e9800998ecf8427e','','','','','',''),(8,'teste5','123456789','202cb962ac59075b964b07152d234b70','4734421282','cosmoskitsune@hotmail.com','Uma empresa ','','SFS',''),(9,'teste6','123456789','202cb962ac59075b964b07152d234b70','4734421282','cosmoskitsune@hotmail.com','Uma empresa ','','SFS',''),(10,'teste7','1234567895','202cb962ac59075b964b07152d234b70','4734421282','cosmoskitsune@hotmail.com','bites','','algum lugar',''),(11,'Shukin','','5797fa2cd8e16663d2636a15a6efd182','','shukin@shukinhekon.com','','','',''),(12,'Shukin1','','d0970714757783e6cf17b26fb8e2298f','','shukin@shukin.com','','','',''),(13,'Shukin2','','d0970714757783e6cf17b26fb8e2298f','','shukin@shukin.com','','','',''),(14,'Shukin3','','d0970714757783e6cf17b26fb8e2298f','','shukin@shukin.com','','','',''),(15,'rodape','12345678951','202cb962ac59075b964b07152d234b70','4734421282','cosmoskitsune@hotmail.com','coisas estranhas','','invisivel',''),(16,'sim','','202cb962ac59075b964b07152d234b70','','email','','','',''),(17,'jabuti','','202cb962ac59075b964b07152d234b70','','lucas@gmail.com','','','',''),(18,'Teste','','698dc19d489c4e4db73e28a713eab07b','','pc@pc.com','','','',''),(19,'raposa','','864f1a5b1b39f5f071ca3123c4e806bb','','cosmoskitsune@hotmail.com','','','',''),(20,'raposacosmica','','864f1a5b1b39f5f071ca3123c4e806bb','34421282','cosmoskitsune@hotmail.com','Uma empresa para os furries','Vinícius Bretas','Rua francisco mascarenhas - 6000','');
-/*!40000 ALTER TABLE `empresa` ENABLE KEYS */;
+INSERT INTO `empresa` (`id_empresa`, `nome`, `cnpj`, `senha`, `telefone`, `email`, `desc_empresa`, `ceo`, `cidade`, `estado`) VALUES
+(1, 'vinicius', 'cnpj', '202cb962ac59075b964b07152d234b70', '', 'cosmoskitsune@hotmail.com', '', '', '', ''),
+(2, 'cosmosgc', '061.480.139-75', '864f1a5b1b39f5f071ca3123c4e806bb', '4734421282', 'cosmoskitsune@hotmail.com', '', '', 'São Francisco do Sul', 'SC'),
+(3, 'teste1', '123456789', '202cb962ac59075b964b07152d234b70', '4734421282', 'cosmoskitsune@hotmail.com', 'empresa com coisas que começa com letra b', '', 'São Francisco do Sul, SC', ''),
+(4, 'teste2', '123456789', '202cb962ac59075b964b07152d234b70', '4734421282', 'cosmoskitsune@hotmail.com', 'Coisas', '', 'SFS', ''),
+(5, 'teste3', '123456789', '202cb962ac59075b964b07152d234b70', '4734421282', 'cosmoskitsune@hotmail.com', 'bush', '', 'R. Francisco Mascarenhas\r\nReta', ''),
+(6, 'teste4', '123456789', '202cb962ac59075b964b07152d234b70', '4734421282', 'cosmoskitsune@hotmail.com', 'bush', '', 'R. Francisco Mascarenhas\r\nReta', ''),
+(7, 'testes', '123456789', 'd41d8cd98f00b204e9800998ecf8427e', '', '', '', '', '', ''),
+(8, 'teste5', '123456789', '202cb962ac59075b964b07152d234b70', '4734421282', 'cosmoskitsune@hotmail.com', 'Uma empresa ', '', 'SFS', ''),
+(9, 'teste6', '123456789', '202cb962ac59075b964b07152d234b70', '4734421282', 'cosmoskitsune@hotmail.com', 'Uma empresa ', '', 'SFS', ''),
+(10, 'teste7', '1234567895', '202cb962ac59075b964b07152d234b70', '4734421282', 'cosmoskitsune@hotmail.com', 'bites', '', 'algum lugar', ''),
+(11, 'Shukin', '', '5797fa2cd8e16663d2636a15a6efd182', '', 'shukin@shukinhekon.com', '', '', '', ''),
+(12, 'Shukin1', '', 'd0970714757783e6cf17b26fb8e2298f', '', 'shukin@shukin.com', '', '', '', ''),
+(13, 'Shukin2', '', 'd0970714757783e6cf17b26fb8e2298f', '', 'shukin@shukin.com', '', '', '', ''),
+(14, 'Shukin3', '', 'd0970714757783e6cf17b26fb8e2298f', '', 'shukin@shukin.com', '', '', '', ''),
+(15, 'rodape', '12345678951', '202cb962ac59075b964b07152d234b70', '4734421282', 'cosmoskitsune@hotmail.com', 'coisas estranhas', '', 'invisivel', ''),
+(16, 'sim', '', '202cb962ac59075b964b07152d234b70', '', 'email', '', '', '', ''),
+(17, 'jabuti', '', '202cb962ac59075b964b07152d234b70', '', 'lucas@gmail.com', '', '', '', ''),
+(18, 'Teste', '', '698dc19d489c4e4db73e28a713eab07b', '', 'pc@pc.com', '', '', '', ''),
+(19, 'raposa', '', '864f1a5b1b39f5f071ca3123c4e806bb', '', 'cosmoskitsune@hotmail.com', '', '', '', ''),
+(20, 'raposacosmica', '', '864f1a5b1b39f5f071ca3123c4e806bb', '34421282', 'cosmoskitsune@hotmail.com', 'Uma empresa para os furries', 'Vinícius Bretas', 'Rua francisco mascarenhas - 6000', ''),
+(21, 'empresa21', '123456789', '202cb962ac59075b964b07152d234b70', '4734421282', 'cosmoskitsune@hotmail.com', 'empresa numero 21, senha 123', 'raposa', 'that is it', '');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `funcionario`
+-- Estrutura da tabela `funcionario`
 --
 
-DROP TABLE IF EXISTS `funcionario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `funcionario` (
+CREATE TABLE IF NOT EXISTS `funcionario` (
   `id_funcionario` int(11) NOT NULL AUTO_INCREMENT,
   `cpf` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `nome` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `telefone` int(15) NOT NULL,
   `senha` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `fk_setor` int(11) NOT NULL,
+  `fk_empresa` int(100) NOT NULL,
   PRIMARY KEY (`id_funcionario`),
-  KEY `fk_setor` (`fk_setor`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `fk_setor` (`fk_setor`),
+  KEY `fk_setor_2` (`fk_setor`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
 --
--- Dumping data for table `funcionario`
+-- Extraindo dados da tabela `funcionario`
 --
 
-/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+INSERT INTO `funcionario` (`id_funcionario`, `cpf`, `nome`, `email`, `telefone`, `senha`, `fk_setor`, `fk_empresa`) VALUES
+(1, '12345678', 'funcionario1', 'func@func.com', 19024857, '202cb962ac59075b964b07152d234b70', 388882, 20),
+(2, '', '', '', 0, 'd41d8cd98f00b204e9800998ecf8427e', 0, 0),
+(3, '12345679', 'funcionario2', 'email@email', 123456798, '202cb962ac59075b964b07152d234b70', 388882, 20),
+(4, '51515151515151515151515151', 'kimmykun', 'kimmykun@live.com', 0, 'c8837b23ff8aaa8a2dde915473ce0991', 388882, 20),
+(5, '691.666.133-74', 'Royoster, mas pode me chamar de Royzin', 'royosterrrrrrrrrapidinho@caudalevantada.sempre', 4666, '26243fdb8fc89675c5598ca0f412c10b', 388883, 20),
+(6, '23242312', 'correberg', 'tf_é_a_meliorcoisa@viciado.emyiff', 2147483647, '81dc9bdb52d04dc20036dbd8313ed055', 388883, 20),
+(7, '', 'Hund', 'diegoborgesdemoraes@gmail.com', 0, 'e2d1ed0fb81f86c7d80dedb32178db5f', 388883, 20),
+(8, '', 'RenanHDMLG', 'renanHDgameplays@SeInscreveAe.com', 0, '4badaee57fed5610012a296273158f5f', 388883, 20);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `projetos`
+-- Estrutura da tabela `niveis`
 --
 
-DROP TABLE IF EXISTS `projetos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `projetos` (
+CREATE TABLE IF NOT EXISTS `niveis` (
+  `id_niveis` int(255) NOT NULL AUTO_INCREMENT,
+  `fk_funcionario` int(255) NOT NULL,
+  `criarServico` tinyint(1) NOT NULL,
+  `verSetores` tinyint(1) NOT NULL,
+  `modificarServiço` tinyint(1) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `ceo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_niveis`),
+  KEY `fk_funcionario` (`fk_funcionario`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Niveis de acesso' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `projetos`
+--
+
+CREATE TABLE IF NOT EXISTS `projetos` (
   `id_projeto` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `descricao` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `horas` int(11) NOT NULL,
   `fk_empresa` int(11) NOT NULL,
   PRIMARY KEY (`id_projeto`),
-  KEY `fk_empresa` (`fk_empresa`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `fk_empresa` (`fk_empresa`),
+  KEY `fk_empresa_2` (`fk_empresa`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `projetos`
+-- Estrutura da tabela `registro`
 --
 
-/*!40000 ALTER TABLE `projetos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `projetos` ENABLE KEYS */;
-
---
--- Table structure for table `registro`
---
-
-DROP TABLE IF EXISTS `registro`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `registro` (
+CREATE TABLE IF NOT EXISTS `registro` (
   `id_registro` int(11) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `dataHoraIni` datetime NOT NULL,
@@ -112,25 +153,19 @@ CREATE TABLE `registro` (
   `fk_servico` int(11) NOT NULL,
   `fk_status` int(11) NOT NULL,
   PRIMARY KEY (`id_registro`),
-  KEY `fk_funcionario` (`fk_funcionario`,`fk_servico`,`fk_status`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `fk_funcionario` (`fk_funcionario`,`fk_servico`,`fk_status`),
+  KEY `fk_funcionario_2` (`fk_funcionario`),
+  KEY `fk_servico` (`fk_servico`),
+  KEY `fk_status` (`fk_status`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `registro`
+-- Estrutura da tabela `servico`
 --
 
-/*!40000 ALTER TABLE `registro` DISABLE KEYS */;
-/*!40000 ALTER TABLE `registro` ENABLE KEYS */;
-
---
--- Table structure for table `servico`
---
-
-DROP TABLE IF EXISTS `servico`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `servico` (
+CREATE TABLE IF NOT EXISTS `servico` (
   `id_servico` int(11) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `horas` int(11) NOT NULL,
@@ -139,69 +174,75 @@ CREATE TABLE `servico` (
   `fk_funcionario` int(11) NOT NULL,
   `fk_setor` int(11) NOT NULL,
   PRIMARY KEY (`id_servico`),
-  KEY `fk_projeto` (`fk_projeto`,`fk_status`,`fk_funcionario`,`fk_setor`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `fk_projeto` (`fk_projeto`,`fk_status`,`fk_funcionario`,`fk_setor`),
+  KEY `fk_projeto_2` (`fk_projeto`),
+  KEY `fk_status` (`fk_status`),
+  KEY `fk_funcionario` (`fk_funcionario`),
+  KEY `fk_setor` (`fk_setor`),
+  FULLTEXT KEY `descricao` (`descricao`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `servico`
+-- Estrutura da tabela `setor`
 --
 
-/*!40000 ALTER TABLE `servico` DISABLE KEYS */;
-/*!40000 ALTER TABLE `servico` ENABLE KEYS */;
-
---
--- Table structure for table `setor`
---
-
-DROP TABLE IF EXISTS `setor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `setor` (
+CREATE TABLE IF NOT EXISTS `setor` (
   `id_setor` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `fk_empresa` int(11) NOT NULL,
   PRIMARY KEY (`id_setor`),
-  KEY `fk_empresa` (`fk_empresa`)
-) ENGINE=MyISAM AUTO_INCREMENT=388887 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `fk_empresa` (`fk_empresa`),
+  KEY `fk_empresa_2` (`fk_empresa`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=388889 ;
 
 --
--- Dumping data for table `setor`
+-- Extraindo dados da tabela `setor`
 --
 
-/*!40000 ALTER TABLE `setor` DISABLE KEYS */;
-INSERT INTO `setor` VALUES (388861,'giga',10),(388862,'mega',10),(388863,'byte',10),(388864,'Música Boa',11),(388865,'Música Ruim',11),(388866,'Música Mediana',11),(388867,'Música Ruim',12),(388868,'Música Ruim',13),(388869,'Música Ruim',14),(388870,'chico bueno',15),(388871,'beringela',15),(388872,'legal',15),(388873,'show',15),(388874,'setorlegal',16),(388875,'',16),(388876,'',16),(388877,'',16),(388878,'',16),(388879,'',17),(388880,'',18),(388881,'',19),(388882,'furry',20),(388883,'discord',20),(388884,'bot',20),(388885,'e-sport',20),(388886,'cursos',20);
-/*!40000 ALTER TABLE `setor` ENABLE KEYS */;
+INSERT INTO `setor` (`id_setor`, `nome`, `fk_empresa`) VALUES
+(388861, 'giga', 10),
+(388862, 'mega', 10),
+(388863, 'byte', 10),
+(388864, 'Música Boa', 11),
+(388865, 'Música Ruim', 11),
+(388866, 'Música Mediana', 11),
+(388867, 'Música Ruim', 12),
+(388868, 'Música Ruim', 13),
+(388869, 'Música Ruim', 14),
+(388870, 'chico bueno', 15),
+(388871, 'beringela', 15),
+(388872, 'legal', 15),
+(388873, 'show', 15),
+(388874, 'setorlegal', 16),
+(388875, '', 16),
+(388876, '', 16),
+(388877, '', 16),
+(388878, '', 16),
+(388879, '', 17),
+(388880, '', 18),
+(388881, '', 19),
+(388882, 'furry', 20),
+(388883, 'discord', 20),
+(388884, 'bot', 20),
+(388885, 'e-sport', 20),
+(388886, 'cursos', 20),
+(388887, 'setorN1', 21),
+(388888, 'setorN2', 21);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `status`
+-- Estrutura da tabela `status`
 --
 
-DROP TABLE IF EXISTS `status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `status` (
+CREATE TABLE IF NOT EXISTS `status` (
   `id_status` int(11) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id_status`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `status`
---
-
-/*!40000 ALTER TABLE `status` DISABLE KEYS */;
-/*!40000 ALTER TABLE `status` ENABLE KEYS */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-09-14 13:46:26
