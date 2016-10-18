2597345f9c58bf5716e3c70ab92acb3d2175e68c
cosmosgc <cosmoskitsune@hotmail.com>
2016-10-18 08:18:15 -0200

    Update menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -216,6 +216,63 @@ $funcionarioCount = $funcionarioCountArray["count(DISTINCT (id_funcionario))"];
 		
                   <li><a href="javascript:void(0)"><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a></li>
 			-->
+			<script>
+				document.getElementById("copyButton").addEventListener("click", function() {
+				    copyToClipboard(document.getElementById("copyTarget"));
+				});
+
+				function copyToClipboard(elem) {
+					  // create hidden text element, if it doesn't already exist
+				    var targetId = "_hiddenCopyText_";
+				    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+				    var origSelectionStart, origSelectionEnd;
+				    if (isInput) {
+					// can just use the original source element for the selection and copy
+					target = elem;
+					origSelectionStart = elem.selectionStart;
+					origSelectionEnd = elem.selectionEnd;
+				    } else {
+					// must use a temporary form element for the selection and copy
+					target = document.getElementById(targetId);
+					if (!target) {
+					    var target = document.createElement("textarea");
+					    target.style.position = "absolute";
+					    target.style.left = "-9999px";
+					    target.style.top = "0";
+					    target.id = targetId;
+					    document.body.appendChild(target);
+					}
+					target.textContent = elem.textContent;
+				    }
+				    // select the content
+				    var currentFocus = document.activeElement;
+				    target.focus();
+				    target.setSelectionRange(0, target.value.length);
+
+				    // copy the selection
+				    var succeed;
+				    try {
+					  succeed = document.execCommand("copy");
+				    } catch(e) {
+					succeed = false;
+				    }
+				    // restore original focus
+				    if (currentFocus && typeof currentFocus.focus === "function") {
+					currentFocus.focus();
+				    }
+
+				    if (isInput) {
+					// restore prior selection
+					elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+				    } else {
+					// clear temporary content
+					target.textContent = "";
+				    }
+				    return succeed;
+				}
+			</script>
+			<input type="text" id="copyTarget" value="<?php echo($id_empresa);?>"> <button id="copyButton">Copy</button><br><br>
+			<input type="text" placeholder="Click here and press Ctrl-V to see clipboard contents">
                 </ul>
               </div>
 
