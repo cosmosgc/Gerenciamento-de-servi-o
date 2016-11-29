9872b400ba60e431d80b951a02ebe94b6eb1fd3a
cosmosgc <cosmoskitsune@hotmail.com>
2016-11-29 16:56:01 -0200

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -1,8 +1,8 @@
 <?php 
 
 //Abrir a conexão com o banco
-//$conexao = mysqli_connect("mysql.hostinger.com.br", "u474781536_admin", "34421282", "u474781536_royal");
-$conexao = mysqli_connect("localhost", "root", "", "royalink");
+$conexao = mysqli_connect("mysql.hostinger.com.br", "u474781536_admin", "34421282", "u474781536_royal");
+//$conexao = mysqli_connect("localhost", "root", "", "royalink");
 if ($conexao == false) {
     $erro = mysqli_connect_error();
     echo ("conectar erro: $erro");
